b99786ee6f24f883a2be3b010c78573479dbc382
Michelle <mchristine.stone91@gmail.com>
2022-03-13 00:14:03 -0500

    Created the Nav component and created an onClick method to listen for clicks on the Nav bar.

---
+++ src/components/Nav/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+function Nav() {
+  const categories = [
+    {
+      name: "Portfolio",
+      description:
+        "An up-to-date portfolio of my highlighed apps and projects!",
+    },
+    {
+      name: "Resume",
+      description: "My current resume and list of my proficiencies.",
+    },
+  ];
+
+  function categorySelected(name) {
+    console.log(`${name} clicked`);
+  }
+
+  return (
+    <header>
+      <h2>
+        <a href="/">Michelle Stone</a>
+      </h2>
+      <nav>
+        <ul className="flex-row">
+          <li className="nav-list">
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <span>Contact</span>
+          </li>
+          {categories.map((category) => (
+            <li key={category.name}>
+                  <span onClick={() => categorySelected(category.name)}>{category.name}</span>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Nav;
