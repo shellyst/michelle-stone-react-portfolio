8fd1059a8a7d05b7f94f08f4a691bcc0612cf4d3
Michelle <mchristine.stone91@gmail.com>
2022-03-14 00:43:24 -0400

    Added basic Footer component.

---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <div className="footer">
+        <a href="https://github.com/shellyst">GitHub</a>
+        <span> | </span>
+        <a href="https://www.linkedin.com/in/michelle-cstone91/">LinkedIn</a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
